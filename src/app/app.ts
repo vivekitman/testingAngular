bf0dc7372880c08d340ea7716ff7c5212a7742c8
Vivek Gupta <vivekitman@gmail.com>
2026-04-20 21:58:14 +0530

    s

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+ 
+import { Card } from './card/card';
 
 @Component({
   selector: 'app-root', 
-  imports: [RouterOutlet],
+  imports: [ Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-   newTodo = signal('');
-   todos = signal<{ text: string; done: boolean }[]>([]);
-
-    AddTodo() {
-      const text = this.newTodo().trim();
-      if (text=='') {return;}
-
-      this.todos.update(todos => [...todos, { text, done: false }]);
-      this.newTodo.set('');
-
-    }
-    ToggleTodo(index: number) {
-      this.todos.update(list => 
-        list.map((item, i) => i === index ? { ...item, done: !item.done } : item)
-         
-      )  ;
-    }
-
-    removeTodo(index: number) {
-      this.todos.update(list => list.filter((_, i) => i !== index));
-    }
+   
 
 }
--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,input} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './card.css',
 })
 export class Card {
+
+  Title = input<string>();
+  Discription = input<string>();
 
 }
