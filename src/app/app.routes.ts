f6e1b5d342e91f99add08507b5e22775f9ef7ea5
Vivek Gupta <vivekitman@gmail.com>
2026-04-22 17:55:58 +0530

    Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComp } from './about-component/home.component';
+import { AboutComp } from './about/about.component';
+import { ContactComp } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{ path: '', component: HomeComp},
+    { path: 'about', component: AboutComp},
+    { path: 'contact', component:ContactComp}
+
+];
