9604d8f93d3c5a0e80f48fdd1537c94a3f85dba8
Vivek Gupta <vivekitman@gmail.com>
2026-05-08 21:28:56 +0530

    POST API

--- src/app/app.ts
+++ src/app/app.ts
@@ -19,10 +19,42 @@ import { User } from './Models/user';
 })
 export class App {
 
-   userService = inject(UserService);
+   //userService = inject(UserService);
 
  
-  users = toSignal<User[]>( this.userService.getUsers())
+ // users = toSignal<User[]>( this.userService.getUsers())
+
+  users = signal<User[]>([]);
+  name = signal<string>('');
+  email = signal<string>('');
+
+  constructor( private userService: UserService){}
+
+  ngOnInit(){
+    this.loadUsers(); // Load users on component initialization
+  }
+
+  loadUsers(){
+    this.userService.getUsers().subscribe( data =>{
+      this.users.set(data);
+    });
+  }
+
+  submitForm(){
+    const payload: User = {
+      name: this.name(),
+      email: this.email(),
+      isActive: false
+    };
+    this.userService.addUser(payload).subscribe( () =>{
+      alert('User added successfully');
+      this.loadUsers(); // Refresh the user list after adding a new user
+      this.name.set('');
+      this.email.set('');
+    })
+  }
+
+  
 
   
 }
